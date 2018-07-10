5928a48c22d5ee59475aaa29a7b72103ed71d46d
Devansh Batra <devanshbatra04@gmail.com>
2018-07-10 16:45:35 +0530

    initialised loader

--- app/src/main/java/com/example/devansh/timepassapp/MainActivity.java
+++ app/src/main/java/com/example/devansh/timepassapp/MainActivity.java
@@ -5,18 +5,28 @@ import android.os.Bundle;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.graphics.Bitmap;
+import android.view.View;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
+    String ShowOrHideWebViewInitialUse = "show";
     private WebView myWebView;
+    private ProgressBar spinner;
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
         myWebView = (WebView)findViewById(R.id.activity_main_webview);
+        spinner = (ProgressBar)findViewById(R.id.progressBar1);
+        myWebView.setWebViewClient(new CustomWebViewClient());
+
         WebSettings webSettings = myWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
         myWebView.loadUrl("http://eventox.herokuapp.com/");
-        myWebView.setWebViewClient(new WebViewClient());
+//        myWebView.setWebViewClient(new WebViewClient());
 
     }
     public void onBackPressed(){
@@ -24,6 +34,28 @@ public class MainActivity extends AppCompatActivity {
             myWebView.goBack();
         } else {
             super.onBackPressed();
+        }
+    }
+    private class CustomWebViewClient extends WebViewClient {
+
+        @Override
+        public void onPageStarted(WebView webview, String url, Bitmap favicon) {
+
+            // only make it invisible the FIRST time the app is run
+            if (ShowOrHideWebViewInitialUse.equals("show")) {
+                webview.setVisibility(webview.INVISIBLE);
+            }
+        }
+
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            ShowOrHideWebViewInitialUse = "hide";
+
+            spinner.setVisibility(View.GONE);
+
+            view.setVisibility(myWebView.VISIBLE);
+            super.onPageFinished(view, url);
+
         }
     }
 
