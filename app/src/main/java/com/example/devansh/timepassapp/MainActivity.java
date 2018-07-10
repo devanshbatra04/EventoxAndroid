601656965a223477f93d454d03b2dc408812956e
Devansh Batra <devanshbatra04@gmail.com>
2018-07-10 16:18:44 +0530

    initial

---
+++ app/src/main/java/com/example/devansh/timepassapp/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.devansh.timepassapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView myWebView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        myWebView = (WebView)findViewById(R.id.activity_main_webview);
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        myWebView.loadUrl("http://eventox.herokuapp.com/");
+        myWebView.setWebViewClient(new WebViewClient());
+
+    }
+    public void onBackPressed(){
+        if(myWebView.canGoBack()) {
+            myWebView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//    }
+}
